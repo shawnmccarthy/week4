9503f1bf1411616aa21e12d4306a1f406f07911f
Shawn McCarthy <shawnmccarthy@Shawns-MacBook-Pro.local>
2018-02-03 11:18:19 -0700

    Second commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.text({
+    type: function(req) {
+        return 'text';
+    }
+}));
+
+app.post('/post', function (req, res) {
+    console.log(req.body);
+    res = res.status(200);
+    if (req.get('Content-Type')) {
+        console.log("Content-Type: " + req.get('Content-Type'));
+        res = res.type(req.get('Content-Type'));
+    }
+    res.send(req.body);
+});
+
+http.createServer(app).listen(8080);
